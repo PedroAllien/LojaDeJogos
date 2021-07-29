d5d16a13d4c91f1727b1854bf33fd9a6c5131cd1
PEDRO FELIPE <pedrofmc2901@gmail.com>
2021-07-29 13:43:50 -0300

    Componente Footer

---
+++ src/components/Footer/styles.tsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  height: 50px;
+  background: #202736;
+  border-top: 5px solid #BAE8F5;
+
+  position: fixed;
+  bottom: 0;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  span{
+    color: #FFF;
+  }
+
+`
--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -3,11 +3,13 @@ import {Link, Redirect} from 'react-router-dom';
 import * as S from './styles';
 
 import Header from '../../components/Header';
+import Footer from '../../components/Footer';
 
 function Home(){
     return (
         <S.Container>
             <Header></Header>
+            <Footer></Footer>
         </S.Container>
     )
 }
